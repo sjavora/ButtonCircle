9da9dd11f88e936962c33eb1ce0a664fc1b9b09a
Šimon Javora <s.javora@me.com>
2019-06-05 21:19:14 -0700

    Initial Commit

---
+++ ButtonCircle/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ButtonCircle
+//
+//  Created by Šimon Javora on 05/06/2019.
+//  Copyright © 2019 Šimon Javora. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
